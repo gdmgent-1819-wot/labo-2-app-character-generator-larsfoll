fb93b4e51a76b8c219cc10ee6d44535423e50a37
follet lars <FolletLars@MacBook-Air-van-follet.local>
2018-10-03 19:08:03 +0200

    Send data to raspberry

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,26 +1,39 @@
+const db = firebase.database();
+
 const colorPicker = document.getElementById('color-picker');
 const characterGeneratorWrapper = document.getElementById('character-generator-wrapper');
+const colorsArray = [];
+for(let i = 1; i <= 64; i++) {
+  colorsArray.push('white');
+}
 
 const clickableGrid = (rows, cols) => {
+  var i = 0;
   const grid = document.createElement('table');
   grid.className = 'grid';
   for (let r = 0; r < rows; ++r) {
     const tr = grid.appendChild(document.createElement('tr'));
     for (let c = 0; c < cols; ++c) {
       const cell = tr.appendChild(document.createElement('td'));
-      cell.addEventListener('click', ((el, r, c) => {
+      ++i;
+      cell.addEventListener('click', ((el, r, c, i) => {
         return () => {
           const chosenColor = colorPicker.value;
           const { backgroundColor } = cell.style;
           cell.style.backgroundColor == '' ?
           cell.style.backgroundColor = `#${chosenColor}` :
           cell.style.backgroundColor = '';
-          submitBtn.backgroundColor = `#${chosenColor}`;
+          setColor(i, cell.style.backgroundColor);
         }
-      })(cell, r, c), false);
+      })(cell, r, c, i), false);
     }
   }
   return grid;
+}
+
+const setColor = (index, color) => {
+  colorsArray[index-1] = color;
+  db.ref('characters/').set({character: colorsArray});
 }
 
 const grid = clickableGrid(8, 8);
--- pi/app_char_generator.py
+++ pi/app_char_generator.py
@@ -1,5 +1,10 @@
 import firebase_admin
 from firebase_admin import credentials
+from firebase_admin import db
 
 cred = credentials.Certificate('../services/firebase.json')
 default_app = firebase_admin.initialize_app(cred)
+
+ref = db.reference('characters')
+
+print(ref.get())
