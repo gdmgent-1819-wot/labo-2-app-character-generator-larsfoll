e274e52537d8d94128e3e9e4e189f242d986c27c
follet lars <FolletLars@MacBook-Air-van-follet.local>
2018-10-03 20:05:26 +0200

    Update

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,9 +2,10 @@ const db = firebase.database();
 
 const colorPicker = document.getElementById('color-picker');
 const characterGeneratorWrapper = document.getElementById('character-generator-wrapper');
+
 const colorsArray = [];
 for(let i = 1; i <= 64; i++) {
-  colorsArray.push('white');
+  colorsArray.push('rgb(255, 255, 255)');
 }
 
 const clickableGrid = (rows, cols) => {
@@ -15,15 +16,17 @@ const clickableGrid = (rows, cols) => {
     const tr = grid.appendChild(document.createElement('tr'));
     for (let c = 0; c < cols; ++c) {
       const cell = tr.appendChild(document.createElement('td'));
+      cell.style.backgroundColor = 'rgb(255, 255, 255)';
       ++i;
       cell.addEventListener('click', ((el, r, c, i) => {
         return () => {
           const chosenColor = colorPicker.value;
-          const { backgroundColor } = cell.style;
-          cell.style.backgroundColor == '' ?
-          cell.style.backgroundColor = `#${chosenColor}` :
-          cell.style.backgroundColor = '';
-          setColor(i, cell.style.backgroundColor);
+          el.style.backgroundColor == 'rgb(255, 255, 255)' ?
+          el.style.backgroundColor = `#${chosenColor}` :
+          el.style.backgroundColor = 'rgb(255, 255, 255)';
+          let firebaseValue = el.style.backgroundColor.substr(4).slice(0, -1);
+          firebaseValue = `[${firebaseValue}]`;
+          setColor(i, firebaseValue);
         }
       })(cell, r, c, i), false);
     }
